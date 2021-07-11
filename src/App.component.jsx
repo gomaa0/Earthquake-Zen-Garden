9facd4051104c0930791a739471caed91b31fb43
Mohamed  Gomaa <m.gomaa.ca@gmail.com>
2021-07-11 15:12:20 -0400

    redesign app component layout and import header component

--- src/App.component.jsx
+++ src/App.component.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from './shared/components/Header/Header.component';
+// eslint-disable-next-line no-unused-vars
+import {defaultProps as headerDefaultProps} from './shared/components/Header/Header.component';
 import './App.styles.scss';
 
 function App() {
-  return <div className="App">My App</div>;
+  return (
+    <>
+      <Header /*{...headerDefaultProps}*/ />
+      <main></main>
+    </>
+  );
 }
 
 export default App;
