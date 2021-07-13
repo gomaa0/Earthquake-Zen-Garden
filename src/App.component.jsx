33e36f18bf5817a1fc0c8614aa1ad0f3f1a2356f
Mohamed  Gomaa <m.gomaa.ca@gmail.com>
2021-07-13 16:38:05 -0400

    config with webpack ready for react

---
+++ src/App.component.jsx
@@ -0,0 +1,11 @@
+import './App.styles.scss'
+
+function App() {
+  return (
+    <div className="App">
+      My App
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  mode: devMode ? 'development': 'production',
+  target: devMode ? 'web': 'browserslist',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader", 
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: '/src/index.html',
+    })
+].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    open: true,
+  }
+};
